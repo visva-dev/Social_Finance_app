1f5b272368199d1b87d48a63f9a964e112803343
Visvaldas <visva.rapalis@gmail.com>
2020-09-12 17:01:31 +0300

    Add basic styling with bootstrap

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,7 @@
-const { environment } = require('@rails/webpacker')
-
-module.exports = environment
+const { environment } = require('@rails/webpacker');
+const webpack = require('webpack');
+environment.plugins.append(
+  'Provide',
+  new webpack.ProvidePlugin({ $: 'jquery', Popper: ['pooper.js', 'default'] })
+);
+module.exports = environment;
