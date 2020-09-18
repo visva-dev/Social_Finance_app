081fb7ffe7a6cc30ebc17a2ff5223842563c3eae
Visvaldas <visva.rapalis@gmail.com>
2020-09-18 18:22:15 +0300

    Add rescue to wrong entered symbols

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,8 +1,18 @@
 class StocksController < ApplicationController
   
   def search
-    @stock = Stock.new_lookup(params[:stock])
-    render 'users/my_portfolio'
+    if params[:stock].present?
+      @stock = Stock.new_lookup(params[:stock])
+      if @stock
+        render 'users/my_portfolio'
+      else
+        flash[:alert] = "Please enter stock symbol"
+        redirect_to my_portfolio_path
+      end 
+    else
+      flash[:alert] = "Wrong stock symbol"
+      redirect_to my_portfolio_path
+    end
   end
   
 end
