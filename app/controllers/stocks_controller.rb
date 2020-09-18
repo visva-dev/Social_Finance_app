c2ebec129c08897916ff33e550f8e84081e7ec60
Visvaldas <visva.rapalis@gmail.com>
2020-09-18 18:02:37 +0300

    Add show stock info in the browser

---
+++ app/controllers/stocks_controller.rb
@@ -0,0 +1,8 @@
+class StocksController < ApplicationController
+  
+  def search
+    @stock = Stock.new_lookup(params[:stock])
+    render 'users/my_portfolio'
+  end
+  
+end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,9 +3,9 @@ class Stock < ApplicationRecord
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
       publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
-      endpoint: 'https://cloud.iexapis.com/v1'
+      endpoint: 'https://sandbox.iexapis.com/v1'
     )
-    client.price(ticker_symbol)
+    new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
   end
   
 end
