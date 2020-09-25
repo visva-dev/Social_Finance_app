89e288bd8eef9fe063b62813e5c86755fe2d786f
Visvaldas <visva.rapalis@gmail.com>
2020-09-25 13:44:50 +0300

    Add friend profile and tracking options

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -7,8 +7,7 @@ class Stock < ApplicationRecord
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
       publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
-      endpoint: 'https://sandbox.iexapis.com/v1'
-    )
+      endpoint: 'https://sandbox.iexapis.com/v1')
     begin
       new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
     rescue => exception
